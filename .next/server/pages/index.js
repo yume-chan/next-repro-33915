e9a0d3e1252414d317ed02936c44c82df0f78da1
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2022-02-04 14:46:20 +0800

    include build cache

---
+++ .next/server/pages/index.js
@@ -0,0 +1,46 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 405;
+exports.ids = [405];
+exports.modules = {
+
+/***/ 778:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Home)
+});
+
+;// CONCATENATED MODULE: external "react/jsx-runtime"
+const jsx_runtime_namespaceObject = require("react/jsx-runtime");
+;// CONCATENATED MODULE: ./value.js
+const value = {};
+
+;// CONCATENATED MODULE: ./pages/index.js
+
+
+function Home() {
+    return(/*#__PURE__*/ jsx_runtime_namespaceObject.jsx("div", {
+        children: value.not.existed.property
+    }));
+};
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(778));
+module.exports = __webpack_exports__;
+
+})();
